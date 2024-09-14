8e452d6f701f9b6b0c06f0a42cb8482faba1de5c
Natalie Chan <natkuma@outlook.com>
2024-09-14 02:19:30 -0400

    updated setting.py database to postgresql

--- backend/sushi_guide/settings.py
+++ backend/sushi_guide/settings.py
@@ -88,11 +88,16 @@ WSGI_APPLICATION = "sushi_guide.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sushi_guide_db',
+        'USER': 'nataliechan',
+        'PASSWORD': 'sushi',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
