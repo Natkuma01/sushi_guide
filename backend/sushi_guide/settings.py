97eb2e51c905967dfb1777e98842f45d6d39d316
Natalie Chan <natkuma@outlook.com>
2026-02-28 22:33:23 -0500

    Fix buttons on image slide

--- backend/sushi_guide/settings.py
+++ backend/sushi_guide/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv(".env.local")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,12 +92,8 @@ WSGI_APPLICATION = "sushi_guide.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sushi_guide_db',
-        'USER': 'nataliechan',
-        'PASSWORD': 'sushi',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
